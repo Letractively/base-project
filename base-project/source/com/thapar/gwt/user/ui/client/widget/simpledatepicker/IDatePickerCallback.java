c0a795848fc90dffda8b393cdc9e3e9f20e0819a
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-04-02 04:27:09 +0000

    Massive updates: file based logger implemented added resource bundle handling added color picker moved date picker packaging (no longer separate module) moved many packages/classes into more suitable homes fixed issues with comment pagination comments/advisory/rating is no longer tied to "File" but rather, to PermissibleObject, allowing more generic use started work to use database persistence of FileData rather than PHP-file-hack created static resource caching jsp (.htaccess mapped urls) for *.cache.html and *.cache.png and anything in public/images (Expires header put in place so it will not even perform a condition GET) added a MimeHelper/StringTokenizer/StringUtils set of classes for general utility use

--- base-project/source/com/thapar/gwt/user/ui/client/widget/simpledatepicker/IDatePickerCallback.java
+++
@@ -1,7 +0,0 @@
-package com.thapar.gwt.user.ui.client.widget.simpledatepicker;
-
-import java.util.Date;
-
-public interface IDatePickerCallback {
-  public void dateSelected(Date date);
-}
--- base-project/source/com/thapar/gwt/user/ui/client/widget/simpledatepicker/IDatePickerDateProvider.java
+++
@@ -1,7 +0,0 @@
-package com.thapar.gwt.user.ui.client.widget.simpledatepicker;
-
-import java.util.Date;
-
-public interface IDatePickerDateProvider {
-  public boolean shouldHighlightDate(Date date);
-}
