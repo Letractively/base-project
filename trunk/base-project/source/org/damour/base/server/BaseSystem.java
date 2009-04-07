d2d27c31453d0191d0bc00f74d10457759bb85ec
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-04-07 00:47:51 +0000

    Added ability to track a class loader (jasperloader doesn't cache like webappclassloader) Moved startup date out of HibernateUtil

--- trunk/base-project/source/org/damour/base/server/BaseSystem.java
+++ trunk/base-project/source/org/damour/base/server/BaseSystem.java
@@ -1,17 +1,37 @@
 package org.damour.base.server;
 
+import java.io.File;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class BaseSystem {
 
-  private static String domainName = null;
-  
+  private static final long startupDate = System.currentTimeMillis();
+  private static String domainName = "" + startupDate;
+  private static boolean isDomainNameSet = false;
+  private static ClassLoader classLoader = null;
+
+  static {
+    try {
+      setBaseClassLoader(BaseSystem.class.getClassLoader());
+    } catch (Throwable t) {
+      Logger.log(t);
+      try {
+        BaseSystem bs = new BaseSystem();
+        setBaseClassLoader(bs.getClass().getClassLoader());
+      } catch (Throwable tt) {
+        Logger.log(tt);
+      }
+    }
+  }
+
   public static String getDomainName() {
     return domainName;
   }
-  
+
   public static String getDomainName(HttpServletRequest request) {
-    if (domainName == null) {
+    if (!isDomainNameSet) {
+      isDomainNameSet = true;
       domainName = request.getServerName();
       if (domainName.lastIndexOf(".") > domainName.indexOf(".")) {
         // remove subdomain
@@ -20,10 +40,29 @@ public class BaseSystem {
     }
     return domainName;
   }
-  
+
   public static void setDomainName(String domainName) {
     BaseSystem.domainName = domainName;
   }
-  
-  
+
+  public static String getTempDir() {
+    return File.separatorChar + "tmp" + File.separatorChar + BaseSystem.getDomainName() + File.separatorChar;
+  }
+
+  public static long getStartupDate() {
+    return startupDate;
+  }
+
+  public static void setBaseClassLoader(final ClassLoader classLoader) {
+    if (BaseSystem.classLoader != classLoader) {
+      Logger.log("BaseSystem:setBaseClassLoader = " + classLoader);
+      Logger.log(new Exception("Debug Stack Dump"));
+      BaseSystem.classLoader = classLoader;
+    }
+  }
+
+  public static ClassLoader getBaseClassLoader() {
+    return classLoader;
+  }
+
 }
