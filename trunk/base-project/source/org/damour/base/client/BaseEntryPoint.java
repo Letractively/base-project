3c4518fa3695a562231a3ed91a245a59fbe9a08d
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-04-07 00:50:50 +0000

    base_settings.properties may now be overridden with base_settings_override.properties.

--- trunk/base-project/source/org/damour/base/client/BaseEntryPoint.java
+++ trunk/base-project/source/org/damour/base/client/BaseEntryPoint.java
@@ -14,23 +14,32 @@ import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
 public class BaseEntryPoint implements EntryPoint, IResourceBundleLoadCallback {
 
+  public static final String BASE_SERVICE_PATH = "servlet/org.damour.base.server.BaseService";
+  public static final String FILE_UPLOAD_SERVICE_PATH = "servlet/org.damour.base.server.FileUploadService";
+  public static final String GET_FILE_SERVICE_PATH = "files/";
+
   private static ResourceBundle settings;
+  private static ResourceBundle settings_override;
   private static ResourceBundle messages;
   private static boolean ready = false;
-  private static String serviceEntryPointDefault = "/servlet/org.damour.base.server.BaseService";
   private static List<IGenericCallback<Void>> startupListeners = new ArrayList<IGenericCallback<Void>>();
 
   public void onModuleLoad() {
     settings = new ResourceBundle("base_settings/", "base_settings", false, new IResourceBundleLoadCallback() {
       public void bundleLoaded(String bundleName) {
-        messages = new ResourceBundle("base_messages/", "base_messages", true, BaseEntryPoint.this);
+        settings_override = new ResourceBundle("base_settings/", "base_settings_override", false, new IResourceBundleLoadCallback() {
+          public void bundleLoaded(String bundleName) {
+            settings.mergeResourceBundle(settings_override);
+            messages = new ResourceBundle("base_messages/", "base_messages", true, BaseEntryPoint.this);
+          }
+        });
       }
     });
-    ((ServiceDefTarget) BaseServiceCache.getServiceUnsafe()).setServiceEntryPoint(serviceEntryPointDefault);
+    ((ServiceDefTarget) BaseServiceCache.getServiceUnsafe()).setServiceEntryPoint(BASE_SERVICE_PATH);
   }
 
   public void bundleLoaded(String bundleName) {
-    String serviceEntryPoint = settings.getString("BaseServiceServiceEntryPoint", serviceEntryPointDefault);
+    String serviceEntryPoint = settings.getString("BaseService", BASE_SERVICE_PATH);
     if (!StringUtils.isEmpty(serviceEntryPoint)) {
       ((ServiceDefTarget) BaseServiceCache.getServiceUnsafe()).setServiceEntryPoint(serviceEntryPoint);
     }
