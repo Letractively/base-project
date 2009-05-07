a732df3ab30e8253aff726d8d361fa3405f060ca
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-05-07 00:54:44 +0000

    Auto-centering of dialogs. Better behavior when center() is called more than once. Ability to turn on/off keyboard events (ENTER/ESC for submission/cancel)

--- trunk/base-project/source/org/damour/base/client/ui/dialogs/PromptDialogBox.java
+++ trunk/base-project/source/org/damour/base/client/ui/dialogs/PromptDialogBox.java
@@ -2,13 +2,14 @@ package org.damour.base.client.ui.dialogs;
 
 import org.damour.base.client.ui.buttons.Button;
 
-import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -23,9 +24,9 @@ public class PromptDialogBox extends DialogBox {
     super(autoHide, modal);
     setText(title);
     Button ok = new Button(okText);
-    ok.addClickListener(new ClickListener() {
+    ok.addClickHandler(new ClickHandler() {
 
-      public void onClick(Widget sender) {
+      public void onClick(ClickEvent event) {
         if (validatorCallback == null || (validatorCallback != null && validatorCallback.validate())) {
           hide();
           if (callback != null) {
@@ -42,9 +43,9 @@ public class PromptDialogBox extends DialogBox {
     }
     if (cancelText != null) {
       Button cancel = new Button(cancelText);
-      cancel.addClickListener(new ClickListener() {
+      cancel.addClickHandler(new ClickHandler() {
 
-        public void onClick(Widget sender) {
+        public void onClick(ClickEvent event) {
           hide();
           if (callback != null) {
             callback.cancelPressed();
@@ -74,23 +75,25 @@ public class PromptDialogBox extends DialogBox {
   }
 
   public boolean onKeyDownPreview(char key, int modifiers) {
-    // Use the popup's key preview hooks to close the dialog when either
-    // enter or escape is pressed.
-    switch (key) {
-    case KeyboardListener.KEY_ENTER:
-      if (validatorCallback == null || (validatorCallback != null && validatorCallback.validate())) {
-        hide();
-        if (callback != null) {
-          callback.okPressed();
+    if (allowKeyboardEvents) {
+      // Use the popup's key preview hooks to close the dialog when either
+      // enter or escape is pressed.
+      switch (key) {
+      case KeyCodes.KEY_ENTER:
+        if (validatorCallback == null || (validatorCallback != null && validatorCallback.validate())) {
+          hide();
+          if (callback != null) {
+            callback.okPressed();
+          }
         }
+        break;
+      case KeyCodes.KEY_ESCAPE:
+        if (callback != null) {
+          callback.cancelPressed();
+        }
+        hide();
+        break;
       }
-      break;
-    case KeyboardListener.KEY_ESCAPE:
-      if (callback != null) {
-        callback.cancelPressed();
-      }
-      hide();
-      break;
     }
     return true;
   }
@@ -105,10 +108,12 @@ public class PromptDialogBox extends DialogBox {
 
       VerticalPanel contentWrapper = new VerticalPanel();
       contentWrapper.setStyleName("dialogContentPanel");
+      contentWrapper.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+      contentWrapper.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
       contentWrapper.add(content);
       contentWrapper.setWidth("100%");
-      //content.getElement().setAttribute("margin", "5px");
-      
+      // content.getElement().setAttribute("margin", "5px");
+
       dialogContent.getFlexCellFormatter().setVerticalAlignment(0, 0, HasVerticalAlignment.ALIGN_MIDDLE);
       dialogContent.getFlexCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
       dialogContent.setWidget(0, 0, contentWrapper);
