a493ba619e26b7e6f45e070e7375b2d3f551c83e
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-06-16 12:52:34 +0000

    Removing fetch of supported_languages... ResourceBundle now does its own check for supported_languages

--- base-project/source/org/damour/base/client/BaseApplication.java
+++ base-project/source/org/damour/base/client/BaseApplication.java
@@ -1,9 +1,7 @@
 package org.damour.base.client;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.damour.base.client.localization.IResourceBundleLoadCallback;
 import org.damour.base.client.localization.ResourceBundle;
@@ -12,7 +10,6 @@ import org.damour.base.client.service.BaseServiceCache;
 import org.damour.base.client.ui.IGenericCallback;
 import org.damour.base.client.ui.authentication.AuthenticationHandler;
 import org.damour.base.client.ui.dialogs.MessageDialogBox;
-import org.damour.base.client.utils.StringTokenizer;
 import org.damour.base.client.utils.StringUtils;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -32,12 +29,8 @@ public class BaseApplication implements EntryPoint, StartupListener {
   private static boolean initialized = false;
   private static List<StartupListener> startupListeners = new ArrayList<StartupListener>();
 
-  private static Map<String, String> supportedLanguages = new HashMap<String, String>();
-
-  private static ResourceBundle settings;
-  private static ResourceBundle settings_override;
+  private static ResourceBundle settings = null;
   private static ResourceBundle messages = null;
-  private static ResourceBundle messages_override = null;
 
   public void onModuleLoad() {
     addStartupListener(this);
@@ -89,11 +82,10 @@ public class BaseApplication implements EntryPoint, StartupListener {
     settings = new ResourceBundle();
     settings.loadBundle("settings/", "settings", false, new IResourceBundleLoadCallback() {
       public void bundleLoaded(String bundleName) {
-        settings_override = new ResourceBundle();
+        final ResourceBundle settings_override = new ResourceBundle();
         settings_override.loadBundle("settings/", "settings_override", false, new IResourceBundleLoadCallback() {
           public void bundleLoaded(String bundleName) {
             settings.mergeResourceBundle(settings_override);
-            setSupportedLanguages(settings.getString("supportedLanguages"));
             String serviceEntryPoint = settings.getString("BaseService", BASE_SERVICE_PATH);
             if (!StringUtils.isEmpty(serviceEntryPoint)) {
               ((ServiceDefTarget) BaseServiceCache.getServiceUnsafe()).setServiceEntryPoint(serviceEntryPoint);
@@ -109,11 +101,9 @@ public class BaseApplication implements EntryPoint, StartupListener {
     // when the bundle is loaded, it will fire an event
     // calling our bundleLoaded
     messages = new ResourceBundle();
-    messages.setSupportedLocales(supportedLanguages);
     messages.loadBundle("messages", "messages", true, new IResourceBundleLoadCallback() {
       public void bundleLoaded(String bundleName) {
-        messages_override = new ResourceBundle();
-        messages_override.setSupportedLocales(supportedLanguages);
+        final ResourceBundle messages_override = new ResourceBundle();
         messages_override.loadBundle("messages", "messages_override", true, new IResourceBundleLoadCallback() {
           public void bundleLoaded(String bundleName) {
             messages.mergeResourceBundle(messages_override);
@@ -155,20 +145,6 @@ public class BaseApplication implements EntryPoint, StartupListener {
 
   public static ResourceBundle getMessages() {
     return messages;
-  }
-
-  private static void setSupportedLanguages(String langStr) {
-    StringTokenizer langs = new StringTokenizer(langStr, ",");
-    for (int i = 0; i < langs.countTokens(); i++) {
-      StringTokenizer langToken = new StringTokenizer(langs.tokenAt(i), "=");
-      String langCode = langToken.tokenAt(0).trim();
-      String langDisplay = langToken.tokenAt(1).trim();
-      supportedLanguages.put(langCode, langDisplay);
-    }
-  }
-
-  public static Map<String, String> getSupportedLanguages() {
-    return supportedLanguages;
   }
 
   // override this
