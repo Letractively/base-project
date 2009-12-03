1744b6bca79910518f55c31e113135f1f934fe85
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-12-03 14:09:04 +0000

    Adding unit test for search. Adding unit test for generic paging (PageHelper)

--- trunk/base-project/test/org/damour/base/server/hibernate/GenericPageTest.java
+++ trunk/base-project/test/org/damour/base/server/hibernate/GenericPageTest.java
@@ -1,11 +1,16 @@
 package org.damour.base.server.hibernate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.damour.base.client.objects.Comment;
 import org.damour.base.client.objects.File;
+import org.damour.base.client.objects.Page;
+import org.damour.base.client.objects.PermissibleObject;
+import org.damour.base.client.objects.Photo;
 import org.damour.base.client.objects.User;
 import org.damour.base.server.hibernate.helpers.GenericPage;
+import org.damour.base.server.hibernate.helpers.PageHelper;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.After;
@@ -51,6 +56,40 @@ public class GenericPageTest {
       }
       session.close();
     }
+  }
+
+  @Test
+  public void genericPageTest() {
+    Session session = HibernateUtil.getInstance().getSession();
+
+    Transaction tx = session.beginTransaction();
+
+    List<Photo> photosList = new ArrayList<Photo>();
+    for (int i = 0; i < 100; i++) {
+      Photo p = new Photo();
+      p.setName("photo" + i);
+      p.setGlobalRead(true);
+      photosList.add(p);
+      session.save(p);
+    }
+    tx.commit();
+
+    for (int pageNumber = 0; pageNumber < 10; pageNumber++) {
+      Page<PermissibleObject> page = PageHelper.getPage(session, Photo.class, null, true, pageNumber, 10);
+
+      List<PermissibleObject> objs = page.getResults();
+      for (PermissibleObject obj : objs) {
+        System.out.print(obj.getName());
+      }
+      System.out.println();
+    }
+
+    tx = session.beginTransaction();
+    for (Photo photo : photosList) {
+      session.delete(photo);
+    }
+    tx.commit();
+    session.close();
   }
 
   @After
