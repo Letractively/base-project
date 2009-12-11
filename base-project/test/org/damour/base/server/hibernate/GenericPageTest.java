cc960122de9ecbd10115c60047d73b60a908902f
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2009-12-11 07:07:48 +0000

    Updated unit tests for search/paging.

--- base-project/test/org/damour/base/server/hibernate/GenericPageTest.java
+++ base-project/test/org/damour/base/server/hibernate/GenericPageTest.java
@@ -1,11 +1,14 @@
 package org.damour.base.server.hibernate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import org.damour.base.client.objects.Comment;
 import org.damour.base.client.objects.File;
 import org.damour.base.client.objects.Page;
+import org.damour.base.client.objects.PageInfo;
 import org.damour.base.client.objects.PermissibleObject;
 import org.damour.base.client.objects.Photo;
 import org.damour.base.client.objects.User;
@@ -14,6 +17,7 @@ import org.damour.base.server.hibernate.helpers.PageHelper;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GenericPageTest {
@@ -35,8 +39,9 @@ public class GenericPageTest {
     for (int i = 0; i < 100; i++) {
       Comment comment = new Comment();
       comment.setComment("comment " + i);
+      comment.setOwner(file.getOwner());
       comment.setAuthor(user);
-      comment.setPermissibleObject(file);
+      comment.setParent(file);
       session.save(comment);
     }
     tx.commit();
@@ -44,7 +49,7 @@ public class GenericPageTest {
 
     for (int testCount = 0; testCount < 10; testCount++) {
       session = HibernateUtil.getInstance().getSession();
-      GenericPage<Comment> pageController = new GenericPage<Comment>(session, Comment.class, 0, 10);
+      GenericPage<Comment> pageController = new GenericPage<Comment>(session, Comment.class, null, 0, 10);
       long pageCount = pageController.getPageCount();
       for (int pageNumber = 0; pageNumber < pageCount; pageNumber++) {
         List<Comment> comments = pageController.getList();
@@ -64,26 +69,32 @@ public class GenericPageTest {
 
     Transaction tx = session.beginTransaction();
 
+    Random r = new Random();
     List<Photo> photosList = new ArrayList<Photo>();
     for (int i = 0; i < 100; i++) {
       Photo p = new Photo();
-      p.setName("photo" + i);
+      p.setName("photox" + i);
       p.setGlobalRead(true);
+      p.setAverageRating(100f * r.nextFloat());
       photosList.add(p);
       session.save(p);
     }
     tx.commit();
 
     for (int pageNumber = 0; pageNumber < 10; pageNumber++) {
-      Page<PermissibleObject> page = PageHelper.getPage(session, Photo.class, null, true, pageNumber, 10);
+      Page<PermissibleObject> page = PageHelper.getPage(session, null, Photo.class, null, "averageRating", false, pageNumber, 10);
 
       List<PermissibleObject> objs = page.getResults();
       for (PermissibleObject obj : objs) {
-        System.out.print(obj.getName());
+        System.out.print(obj.getName() + ":" + obj.getAverageRating() + " ");
       }
       System.out.println();
     }
 
+    PageInfo pageInfo = PageHelper.getPageInfo(session, null, Photo.class, null, 10);
+    System.out.println("Row Count: " + pageInfo.getTotalRowCount());
+    System.out.println("Last Page: " + pageInfo.getLastPageNumber());
+    
     tx = session.beginTransaction();
     for (Photo photo : photosList) {
       session.delete(photo);
@@ -92,11 +103,23 @@ public class GenericPageTest {
     session.close();
   }
 
+  @BeforeClass
+  public static void before() {
+    System.out.println("*********** before begin ***********");
+    HashMap<String, String> overrides = new HashMap<String, String>();
+    overrides.put("showSQL", "true");
+    overrides.put("hbm2ddlMode", "update");
+    overrides.put("tablePrefix", "test_");
+    HibernateUtil.resetHibernate();
+    HibernateUtil.getInstance(overrides);
+    System.out.println("*********** before end ***********");
+  }
+
   @After
   public void after() {
     System.out.println("*********** after begin ***********");
     HibernateUtil.getInstance().getSessionFactory().close();
-    HibernateUtil.getInstance().resetHibernate();
+    HibernateUtil.resetHibernate();
     System.out.println("*********** after end ***********");
   }
 
