717e456158772ecc83f1f1d214e24fb36ea4ca3f
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2011-07-04 06:21:09 +0000

    Tag is now a full-blown PermissibleObject

--- base-project/source/org/damour/base/client/objects/Tag.java
+++ base-project/source/org/damour/base/client/objects/Tag.java
@@ -1,47 +1,10 @@
 package org.damour.base.client.objects;
 
-import java.io.Serializable;
+public class Tag extends PermissibleObject {
 
-public class Tag implements Serializable, IHibernateFriendly {
-
-  public Long id;
-  public String name;
-  public String description;
   public Tag parentTag;
 
   public Tag() {
-  }
-
-  public String getCachePolicy() {
-    return "nonstrict-read-write";
-  }
-
-  public boolean isLazy() {
-    return false;
-  }
-
-  public boolean isFieldUnique(String fieldName) {
-    return false;
-  }
-
-  public boolean isFieldKey(String fieldName) {
-    return false;
-  }
-
-  public String getFieldType(String fieldName) {
-    return null;
-  }
-
-  public int getFieldLength(String fieldName) {
-    return -1;
-  }
-  
-  public String getSqlUpdate() {
-    return null;
-  }
-
-  public boolean isFieldMapped(String fieldName) {
-    return true;
   }
 
   public Tag getParentTag() {
@@ -52,27 +15,4 @@ public class Tag implements Serializable, IHibernateFriendly {
     this.parentTag = parentTag;
   }
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
 }
