e54ce82aa5242dd9b7ef160f762ce657be4815a9
mdamour1976 <mdamour1976@83febac2-c655-11dd-aa7b-9fdd872f0417>
2012-08-19 02:26:38 +0000

    Tweaked privacy dialog - i18n issue Simplified use of email tooling

--- base-project/source/org/damour/base/server/GmailHelper.java
+++ base-project/source/org/damour/base/server/GmailHelper.java
@@ -49,6 +49,12 @@ public class GmailHelper implements IEmailService {
     sendMessage(BaseSystem.getSmtpHost(), from, from, to, subject, message);
   }
 
+  public boolean sendMessage(String to, String subject, String message) {
+    String from = BaseSystem.getAdminEmailAddress();
+    message = "<BR/>" + message + "<BR/>";
+    return sendMessage(BaseSystem.getSmtpHost(), from, from, to, subject, message);
+  } 
+  
   public boolean sendMessage(String smtpHost, String fromAddress, String fromName, String to, String subject, String text) {
     try {
       Properties props = new Properties();
